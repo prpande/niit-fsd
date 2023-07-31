ba24dc1c1907e0a012f194b24176463bc245cedb
Pratyush Pande <pratyush.pande@gmail.com>
2023-07-31 15:44:20 +0530

    Adding notes, concept slides & demo src

---
+++ C1-Programming Foundation Using Java/S5-functions3107/src/session/PrintName.java
@@ -0,0 +1,18 @@
+package session;
+
+/*
+ * Program to print name using a function.
+ */
+public class PrintName
+{
+    public void print()
+    {
+        System.out.println("John Doe");
+    }
+
+    public static void main(String[] args)
+    {
+        PrintName obj = new PrintName();
+        obj.print();
+    }
+}
