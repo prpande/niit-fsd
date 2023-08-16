0bd9193b74620740b8d4a8e6d7ab894e7a2fe150
Pratyush Pande <pratyush.pande@gmail.com>
2023-08-16 18:03:15 +0530

    Adding practice solution

--- C2-Java Language Fundamentals/S5-StringManipulation-1608/src/practice/reverse-string/soln/src/main/java/com/jap/ReverseStringDemo.java
+++ C2-Java Language Fundamentals/S5-StringManipulation-1608/src/practice/reverse-string/soln/src/main/java/com/jap/ReverseStringDemo.java
@@ -1,21 +1,30 @@
 package com.jap;
 
-public class ReverseStringDemo {
-    public static void main(String[] args) {
-
-
-
+public class ReverseStringDemo 
+{
+    public static void main(String[] args) 
+    {
         //Create a String object.
+        String str = "Hello World!";
 
         //Create an object of the class ReverseStringDemo
+        ReverseStringDemo obj = new ReverseStringDemo();
 
         //Call the method getReverseString() and pass the parameter
-
-
+        String reverseString = obj.getReverseString(str);
+        System.out.println(reverseString);
     }
-//Write the logic to reverse the String  inside the below method and return the reverse String.
-public String getReverseString(String string){
 
-        return "";
+    //Write the logic to reverse the String  inside the below method and return the reverse String.
+    public String getReverseString(String string)
+    {
+        String reverseStr = "";
+        int length = string.length();
+        for(int i = length - 1; i >=0 ; i--)
+        {
+            reverseStr = reverseStr + string.charAt(i);
+        }
+
+        return reverseStr;
     }
 }
--- C2-Java Language Fundamentals/S5-StringManipulation-1608/src/practice/shortest-word/soln/src/main/java/com/jap/ShortestWord.java
+++ C2-Java Language Fundamentals/S5-StringManipulation-1608/src/practice/shortest-word/soln/src/main/java/com/jap/ShortestWord.java
@@ -1,20 +1,37 @@
 package com.jap;
-
-
-public class ShortestWord {
-    public static void main(String[] args) {
-
+public class ShortestWord 
+{
+    public static void main(String[] args) 
+    {
         //Declare and initialize a String array.
+        String[] array = {"Hello", "world", "is", "easy"};
 
         // Create an object of the class ShortestWord
+        ShortestWord obj = new ShortestWord();
 
         //Call the method findShortestWord() and pass the array.
+        String shortestWord = obj.findShortestWord(array);
+        System.out.println("Shortest word: " + shortestWord);
     }
 
     //Write the logic to find the shortest word from the String array inside
     // the below method and return the shortest word.
-    public String findShortestWord(String[] words) {
+    public String findShortestWord(String[] words) 
+    {
+        if(words == null || words.length < 1)
+        {
+            return "Cannot find shortest word as array is empty";
+        }
 
-        return "";
+        String shortestString = words[0];
+        for(String str : words)
+        {
+            if(str.length() < shortestString.length())
+            {
+                shortestString = str;
+            }
+        }
+
+        return shortestString;
     }
 }
