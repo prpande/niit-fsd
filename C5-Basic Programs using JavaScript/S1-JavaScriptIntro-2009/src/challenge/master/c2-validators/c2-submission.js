a04cb5c4d113753535df245ba2c49a2740aa5660
Pratyush Pande <pratyush.pande@gmail.com>
2023-09-20 21:21:43 +0530

    Intro to JS challenge boilerplate.

---
+++ C5-Basic Programs using JavaScript/S1-JavaScriptIntro-2009/src/challenge/master/c2-validators/c2-submission.js
@@ -0,0 +1,33 @@
+/*
+    Challenge 2 - Validate Values
+*/
+
+let firstName = "James";
+let lastName = "Smith";
+let age = 34;
+let isMarried = true;
+let city = "San Jose";
+let state = "California";
+let postalCode = "CA";
+
+// Provide solution code here for Validating FirstName
+
+
+// Provide solution code here for Validating LastName
+
+
+// Provide solution code here for Validating Age
+
+
+// Provide solution code here for Validating isMarried
+
+
+// Provide solution code here for Validating City
+
+
+// Provide solution code here for Validating State
+
+ 
+// Provide solution code here for Validating Postal Code
+
+ 
---
+++ C5-Basic Programs using JavaScript/S1-JavaScriptIntro-2009/src/challenge/master/c3-lucky-number/c3-submission.js
@@ -0,0 +1,11 @@
+/*
+    Challenge 3 - Know Your Lucky Number
+*/ 
+
+// Declare and initialize variable to store date in `mmddyyyy` format
+
+// Declare variable to store single digit sum value
+
+// Write nested loop to calculate single digit sum of the date stored in `mmddyyyy` format
+
+// Display the single digit sum, which is the lucky number based on the date stored
