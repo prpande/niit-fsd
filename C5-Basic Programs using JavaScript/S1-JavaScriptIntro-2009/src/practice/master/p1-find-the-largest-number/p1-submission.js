d9517499e34ddb64777f1bfc8be14247d7d15a23
Pratyush Pande <pratyush.pande@gmail.com>
2023-09-20 21:14:48 +0530

    Intro to JS practice boilerplate

---
+++ C5-Basic Programs using JavaScript/S1-JavaScriptIntro-2009/src/practice/master/p1-find-the-largest-number/p1-submission.js
@@ -0,0 +1,20 @@
+/*
+    Practice 1 - Find the Largest Number​
+*/
+
+// Declare and initialize variables
+
+
+// Declare a variable to store the resultant largest number
+
+
+// Write logic to find the largest number using nested-if
+
+
+// Display the largest number
+
+
+// Write logic to find the largest number using ternary operator
+
+
+// Display the largest number
---
+++ C5-Basic Programs using JavaScript/S1-JavaScriptIntro-2009/src/practice/master/p3-fibonacci-first-10/p3-submission.js
@@ -0,0 +1,11 @@
+/*
+    Practice 3 - Fibonacci Series for First 10 numbers
+*/ 
+
+// Declare and initialize variable to store count value.
+
+// Declare and initialize variables to store the first two values of the fibonacci series.
+
+// Display the first two values of the fibonacci series.
+
+// Write `for` loop to generate rest of the values in the fibonacci series.
