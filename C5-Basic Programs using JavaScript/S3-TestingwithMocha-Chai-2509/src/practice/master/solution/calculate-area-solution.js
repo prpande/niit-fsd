e3dcfd618254a966aca3eab1089d365aea4e8908
Pratyush Pande <pratyush.pande@gmail.com>
2023-09-26 09:46:53 +0530

    JS unit testing practice boilerplate

---
+++ C5-Basic Programs using JavaScript/S3-TestingwithMocha-Chai-2509/src/practice/master/solution/calculate-area-solution.js
@@ -0,0 +1,14 @@
+// copy the solution code from `Practice 1 - Calculate Area` 
+// from the sprint `Implement Modular Programming using Functions`
+
+
+
+
+
+
+// do not delete the below code, it is written to export the functions to be tested
+module.exports = {
+   calculateAreaOfCircle,
+   calculateAreaOfRectangle,
+   calculateAreaOfSquare
+}
---
+++ C5-Basic Programs using JavaScript/S3-TestingwithMocha-Chai-2509/src/practice/master/test/calculate-area-spec.js
@@ -0,0 +1,1 @@
+// Write test code here
