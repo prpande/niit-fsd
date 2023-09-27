382c704dcfa1243f3a8f25c98dfa7e56fdc9f6ef
Pratyush Pande <pratyush.pande@gmail.com>
2023-09-27 17:12:14 +0530

    JS array challenge master

---
+++ C6-Interactive Web Pages Using Modern JavaScript/S1-JSArrays-2709/src/challenge/master/percentile-solution.js
@@ -0,0 +1,16 @@
+
+
+//function to sort the array of marks passed as parameter
+function sortArray() {
+
+}
+
+//function to compute and return the percentile array
+function calculatePercentileArray() {
+}
+
+// do not delete below code, it is written to make the functions exportable for testing purpose
+module.exports = {
+sortArray,
+calculatePercentileArray
+}
