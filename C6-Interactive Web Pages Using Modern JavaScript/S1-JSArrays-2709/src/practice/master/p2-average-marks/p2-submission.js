b23abf7848d05d3e2be734d12e5fdd7d3b91d4a7
Pratyush Pande <pratyush.pande@gmail.com>
2023-09-27 16:49:06 +0530

    JS arrays practice master

---
+++ C6-Interactive Web Pages Using Modern JavaScript/S1-JSArrays-2709/src/practice/master/p2-average-marks/p2-submission.js
@@ -0,0 +1,9 @@
+
+//Calculate the average of array of marks passed as parameter
+function calculateAverage() {
+}
+
+// do not delete below code, it is written to make the functions exportable for testing purpose
+module.exports = {
+    calculateAverage
+}
---
+++ C6-Interactive Web Pages Using Modern JavaScript/S1-JSArrays-2709/src/practice/master/p4-sort-marks/p4-submission.js
@@ -0,0 +1,10 @@
+
+//Sort the marks from an array of marks passed as parameter
+function sortMarks(){
+
+}
+
+// do not delete below code, it is written to make the functions exportable for testing purpose
+module.exports = {
+    sortMarks
+}
