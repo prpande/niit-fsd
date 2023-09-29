dbed0d53773afd39bc288e3ef5ed4175b0727cc0
Pratyush Pande <pratyush.pande@gmail.com>
2023-09-29 18:33:33 +0530

    JS array function practice master

---
+++ C6-Interactive Web Pages Using Modern JavaScript/S2-JSArrayFunctions-2909/src/practice/master/p3-product-stock/p3-submission.js
@@ -0,0 +1,11 @@
+
+
+//Find the inventory of each product type using arrow functions
+//Pass the products as parameter to the function
+function findProductInventory(){
+}
+
+// do not delete below code, it is written to make the functions exportable for testing purpose
+module.exports = {
+    findProductInventory
+}
