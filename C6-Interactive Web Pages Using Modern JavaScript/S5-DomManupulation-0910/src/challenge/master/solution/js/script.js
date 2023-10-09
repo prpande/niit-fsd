7f34d810d8d827c8f0cc710c709eb3337430e003
Pratyush Pande <pratyush.pande@gmail.com>
2023-10-09 14:45:46 +0530

    DOM manipulation challenge master

---
+++ C6-Interactive Web Pages Using Modern JavaScript/S5-DomManupulation-0910/src/challenge/master/solution/js/script.js
@@ -0,0 +1,7 @@
+// Write solution code here to dynamically add the form fields 
+
+// define funtion submitOrder() to save the order details on clicking the submit button
+
+
+// do not delete the code given below, it is written to export the functions to be tested
+module.exports = submitOrder;
