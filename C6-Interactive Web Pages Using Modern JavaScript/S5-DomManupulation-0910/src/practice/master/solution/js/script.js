a26a583362149464aa63fd6d84a24c07bb5e8311
Pratyush Pande <pratyush.pande@gmail.com>
2023-10-09 14:41:04 +0530

    DOM manipulation practice master

---
+++ C6-Interactive Web Pages Using Modern JavaScript/S5-DomManupulation-0910/src/practice/master/solution/js/script.js
@@ -0,0 +1,28 @@
+let noteList = [];
+
+let view = "grid"; //default view is grid-view
+
+//task-1 : define function `saveNote()` to add note
+
+
+//task-2 : define function `displayNotes()` to display all notes
+
+
+ 
+//task-3 : delete note - This task is Optional
+function deleteNote() {
+    
+}
+ 
+//task-4 : toggle note view - This task is Optional
+function toggleView() {
+    
+}
+
+// do not delete the code given below, it is written to export the functions to be tested
+module.exports = {
+    saveNote,
+    displayNotes,
+    deleteNote,
+    toggleView
+}
