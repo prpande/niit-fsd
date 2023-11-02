2629852b7e283ddb104b90d912e334969bac91f8
Pratyush Pande <pratyush.pande@gmail.com>
2023-11-02 22:47:50 +0530

    Angular services sprint challenge solution

---
+++ C7-Building Single Page Applications Using Angular/S5-AngularServices-2510/src/challenge/soln/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NoteViewComponent } from './note-view/note-view.component';
+import { NoteAddComponent } from './note-add/note-add.component';
+import { SearchComponent } from './search/search.component';
+import { NoteComponent } from './note/note.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NoteViewComponent,
+    NoteAddComponent,
+    SearchComponent,
+    NoteComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
