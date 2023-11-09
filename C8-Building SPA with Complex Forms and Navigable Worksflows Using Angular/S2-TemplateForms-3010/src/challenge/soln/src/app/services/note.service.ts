7d576fb8eb41a8f051a8a8e4d4ccfce96c7523ba
Pratyush Pande <pratyush.pande@gmail.com>
2023-11-10 00:51:24 +0530

    Template forms sprint challenge solution

---
+++ C8-Building SPA with Complex Forms and Navigable Worksflows Using Angular/S2-TemplateForms-3010/src/challenge/soln/src/app/services/note.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Note } from '../models/note';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  note_url: string = "http://localhost:3000/notes"
+  constructor(private http: HttpClient) { }
+
+  getNotes(): Observable<Note[]>{
+    return this.http.get<Note[]>(this.note_url);
+  }
+
+  addNote(note: Note): Observable<Note>{
+    return this.http.post<Note>(this.note_url, note);
+  }
+}
