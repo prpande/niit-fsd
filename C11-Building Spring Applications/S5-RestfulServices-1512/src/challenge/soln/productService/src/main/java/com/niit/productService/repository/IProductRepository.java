6242d418c3368933753821256d7834bcb76511f6
Pratyush Pande <pratyush.pande@gmail.com>
2023-12-17 17:10:00 +0530

    Rest sprint adding repository annotation

--- C11-Building Spring Applications/S5-RestfulServices-1512/src/challenge/soln/productService/src/main/java/com/niit/productService/repository/IProductRepository.java
+++ C11-Building Spring Applications/S5-RestfulServices-1512/src/challenge/soln/productService/src/main/java/com/niit/productService/repository/IProductRepository.java
@@ -3,5 +3,6 @@ package com.niit.productService.repository;
 import com.niit.productService.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface IProductRepository extends CrudRepository<Product, Integer> {
 }
--- C11-Building Spring Applications/S5-RestfulServices-1512/src/practice/soln/babyimdb/src/main/java/com/niit/babyimdb/repository/IMovieRepository.java
+++ C11-Building Spring Applications/S5-RestfulServices-1512/src/practice/soln/babyimdb/src/main/java/com/niit/babyimdb/repository/IMovieRepository.java
@@ -3,5 +3,6 @@ package com.niit.babyimdb.repository;
 import com.niit.babyimdb.domain.Movie;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface IMovieRepository extends CrudRepository<Movie, Integer> {
 }
