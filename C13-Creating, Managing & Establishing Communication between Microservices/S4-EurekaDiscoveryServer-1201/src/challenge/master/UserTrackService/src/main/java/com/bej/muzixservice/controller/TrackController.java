a40f7b7caa18db5b63841f98a0a8f5424d39b93f
Pratyush Pande <pratyush.pande@gmail.com>
2024-01-14 13:14:24 +0530

    Eureks server sprint challenge boilerplate

---
+++ C13-Creating, Managing & Establishing Communication between Microservices/S4-EurekaDiscoveryServer-1201/src/challenge/master/UserTrackService/src/main/java/com/bej/muzixservice/controller/TrackController.java
@@ -0,0 +1,64 @@
+package com.bej.muzixservice.controller;
+
+import com.bej.muzixservice.domain.Track;
+import com.bej.muzixservice.domain.User;
+import com.bej.muzixservice.exception.TrackAlreadyExistsException;
+import com.bej.muzixservice.exception.TrackNotFoundException;
+import com.bej.muzixservice.exception.UserAlreadyExistsException;
+import com.bej.muzixservice.service.TrackServiceImpl;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v2")
+public class TrackController {
+    // Auto wire the service layer object
+    @PostMapping("register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) throws UserAlreadyExistsException {
+        // Register a new user and save to db, return 201 status if user is saved else 500 status
+        return null;
+    }
+
+    @PostMapping("user/track")
+    public ResponseEntity<?> saveTrack(@RequestBody Track track, HttpServletRequest request) throws TrackAlreadyExistsException {
+        // add a track to a specific user, return 201 status if track is saved else 500 status
+
+        return null;
+    }
+
+    @GetMapping("user/tracks")
+    public ResponseEntity<?> getAllTracks(HttpServletRequest request) {
+
+        // display all the tracks of a specific user, extract user id from claims,
+        // return 200 status if success else return 500 status
+
+      return null;
+    }
+
+    @DeleteMapping("user/track/{trackId}")
+    public ResponseEntity<?> deleteTrack(@PathVariable String trackId,HttpServletRequest request) throws TrackNotFoundException {
+        // delete a track based on user id and track id, extract user id from claims
+        // return 200 status if deleted else return 500 status
+
+      return null;
+    }
+
+    @PutMapping("user/track")
+    public ResponseEntity<?> updateTrack(@RequestBody Track track,HttpServletRequest request){
+        // update a track based on user id and track id, extract user id from claims
+        // return 200 status if updated else return 500 status
+
+       return null;
+    }
+    private String getUserIdFromClaims(HttpServletRequest request){
+        Claims claims = (Claims) request.getAttribute("claims");
+        System.out.println("User ID from claims :: " + claims.getSubject());
+        return claims.getSubject();
+    }
+}
---
+++ C13-Creating, Managing & Establishing Communication between Microservices/S4-EurekaDiscoveryServer-1201/src/challenge/master/UserTrackService/src/main/java/com/bej/muzixservice/service/TrackServiceImpl.java
@@ -0,0 +1,55 @@
+package com.bej.muzixservice.service;
+
+import com.bej.muzixservice.domain.Track;
+import com.bej.muzixservice.domain.User;
+import com.bej.muzixservice.exception.TrackAlreadyExistsException;
+import com.bej.muzixservice.exception.TrackNotFoundException;
+import com.bej.muzixservice.exception.UserAlreadyExistsException;
+import com.bej.muzixservice.exception.UserNotFoundException;
+
+import com.bej.muzixservice.repository.UserTrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrackServiceImpl implements ITrackService{
+
+    // Autowire the UserTrackRepository using constructor autowiring
+
+
+
+
+    @Override
+    public User registerUser(User user) throws UserAlreadyExistsException {
+        // Register a new user
+    return null;
+    }
+
+    @Override
+    public User saveUserTrackToWishList(Track track, String userId) throws TrackAlreadyExistsException, UserNotFoundException {
+        // Save the tracks to the play list of user.
+        return null;
+    }
+
+    @Override
+    public List<Track> getAllUserTracksFromWishList(String userId) throws Exception {
+        // Get all the tracks for a specific user
+        return null;
+
+    }
+
+    @Override
+    public User deleteTrack(String userId, String trackId) throws TrackNotFoundException, UserNotFoundException {
+        // delete the user track details specified
+        return null;
+    }
+
+
+    @Override
+    public User updateUserTrackWishListWithGivenTrack(String userId, Track track) throws UserNotFoundException, TrackNotFoundException, TrackAlreadyExistsException {
+        // Update the specific track details
+        return null;
+    }
+}
