e2b9f5bb50e75a9b98826830bd601d2f830be95f
Pratyush Pande <pratyush.pande@gmail.com>
2024-01-14 13:02:18 +0530

    Eureka server sprint demo code

---
+++ C13-Creating, Managing & Establishing Communication between Microservices/S4-EurekaDiscoveryServer-1201/src/session/demo-1-streaming-application/SpringCloudAPIGateway/src/main/java/com/bej/springcloudapigateway/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.bej.springcloudapigateway.config;
+
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/api/v1/**")
+                        .uri("lb://user-authentication-service"))
+                .route(p->p
+                .path("/api/v2/user/**","/api/v2/register")
+                        .uri("lb://user-movie-service"))
+                .build();
+    }
+
+
+
+
+}
+
+
+
+
