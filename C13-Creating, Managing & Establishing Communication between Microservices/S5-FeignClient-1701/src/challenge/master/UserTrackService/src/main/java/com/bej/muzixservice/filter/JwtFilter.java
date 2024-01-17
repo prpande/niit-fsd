989fac13ec96061eddd536d65528cb61cf20769a
Pratyush Pande <pratyush.pande@gmail.com>
2024-01-17 11:55:46 +0530

    Feign client sprint challenge boilerplate

---
+++ C13-Creating, Managing & Establishing Communication between Microservices/S5-FeignClient-1701/src/challenge/master/UserTrackService/src/main/java/com/bej/muzixservice/filter/JwtFilter.java
@@ -0,0 +1,23 @@
+package com.bej.muzixservice.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        ServletOutputStream pw = response.getOutputStream();
+        // Parse and validate the token and set the user id from claims in the request header as an attribute.
+        //expects the token to come from the header
+            filterChain.doFilter(request, response);
+        }
+    }
+
